5497ee4450a504f1621ffe9b27e0234f1d78c25f
Hard <Hard>
2016-09-20 16:57:48 +0800

    add member file

---
+++ python_basic/member.py
@@ -0,0 +1,12 @@
+# -*- coding:UTF-8 -*-
+#检查用户名和PIN码
+
+database = [['alber', '1234'],['dilber','4242'],['smith','7524'],['jones','9843']]
+username = raw_input('User name:')
+pin = raw_input('PIN code:')
+
+if [username, pin] in database:
+    print 'Access granted'
+else:
+    print 'Access deny'
+
